a573d5c2d9ecff012cf245f943005d90be317525
Cassiar <cassiardylanbeaver@gmail.com>
2023-02-25 16:36:08 -0500

    updated buffer structs for raytracing

--- BufferStructs.h
+++ BufferStructs.h
@@ -21,3 +21,15 @@ struct PixelShaderExternalData {
 	int lightCount;
 	Light lights[MAX_LIGHTS];
 };
+
+//world scene data, all rays need it
+struct RaytracingSceneData {
+	DirectX::XMFLOAT4X4 inverseViewProjection;
+	DirectX::XMFLOAT3 cameraPosition;
+};
+
+//must match raytracing shader define
+#define MAX_INSTANCES_PER_BLAS 100
+struct RaytracingEntityData {
+	DirectX::XMFLOAT4 color[MAX_INSTANCES_PER_BLAS];
+};
